2f96fabacf55317f9279188a5e38ec44fbf9aa8a
sixinyi <yangwenxuans@outlook.com>
2019-10-22 14:15:41 -0400

    10/22 Bruce AddToPositiveOrNegativeSum

---
+++ src/AddToPositiveOrNegativeSum.java
@@ -0,0 +1,13 @@
+// 10.22 Bruce this program can execute 10 times.
+import java.util.Scanner;
+public class AddToPositiveOrNegativeSum {
+    public static void main(String[] args)    {
+        Scanner input=new Scanner(System.in);
+        int
+    }
+        for (int i = 1; i <= 10; i++)        {
+            /* in this block, allow the user to input a number and add the number to the appropriate sum */
+        }
+        //output the sums    }
+    }
+
