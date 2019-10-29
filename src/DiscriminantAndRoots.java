9c3d5bc34263ae5d7aadc7f9df7f665098e54198
sixinyi <yangwenxuans@outlook.com>
2019-10-29 11:22:35 -0400

    10/29 Bruce

---
+++ src/DiscriminantAndRoots.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class DiscriminantAndRoots {
+    public static  void main(String[] args){
+        Scanner input=new Scanner(System.in);
+        System.out.println("Enter a: ");
+        int a=input.nextInt();
+        System.out.println("Enter b: ");
+        int b=input.nextInt();
+        System.out.println("Enter c: ");
+        int c=input.nextInt();
+
+        System.out.println(roots(a, b, c));
+
+    }
+
+    public static String roots(int a, int b, int c){
+
+    }
+
+}
