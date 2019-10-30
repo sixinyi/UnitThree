9797b5e6fdc30e8662c80aa541ade0378b868912
sixinyi <yangwenxuans@outlook.com>
2019-10-30 17:54:58 -0400

    10/30 Bruce TrickyDice

--- src/TrickyDice.java
+++ src/TrickyDice.java
@@ -1,8 +1,11 @@
+// Bruce 10.30 make a program that create two random number.
 public class TrickyDice {
+
     private static void regularRoll(){
         int num = (int)(Math.random()*6)+1;
         System.out.println(num);
     }
+
     private static void trickyRoll(){
         double value = (int) (Math.random() * 100) + 1;
         if (value< 17)
@@ -18,8 +21,10 @@ public class TrickyDice {
         else if (value>= 85)
             System.out.println("6");
     }
+
     public static void main(String[] args) {
         regularRoll();
         trickyRoll();
     }
+
 }
